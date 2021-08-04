12934a60a6938599b28e6d85795798869df2f784
bluehome.wu <bluehome.wu@gmail.com>
2021-08-04 20:46:24 +0800

    Z01R: Overlay: Add Overlay-havoc

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-havoc \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
