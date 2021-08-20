831c63f3a054bf09681914fc863630c98fd1bf07
Aryan Arora <arora.aryan7@gmail.com>
2021-08-20 07:24:28 +0000

    Revert "Z01R: Switch to power-libperfmgr HAL"
    * It borks dt2w
    
    This reverts commit 5bac9fc21300071d4eca87e803f40731c230a5f5.

--- device.mk
+++ device.mk
@@ -382,7 +382,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.qcom:64
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
